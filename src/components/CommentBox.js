09573156c15558d62a0a2908974c42971b029490
taha uygun <tahauygun@gmail.com>
2020-02-05 11:41:51 -0800

    adding action and reducer tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = {comment: ''}
@@ -7,6 +9,7 @@ class CommentBox extends Component {
   }
   handleSubmit=event=>{
     event.preventDefault();
+    this.props.saveComment(this.state.comment);
     this.setState({comment: ''})
   }
 
@@ -23,4 +26,4 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(null, actions)(CommentBox);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {Provider} from 'react-redux';
-import {createStore} from "redux";
-import reducers from 'reducers';
-
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "reducers";
+import Root from 'Root';
 import App from "components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Root>
+    <App />
+  </Root>,
+  document.querySelector("#root")
+);
